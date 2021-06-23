22f0bbb5977d98c2593a2f61fde6b454ea314a65
Yan Lyra <yan.lyra@coopers.pro>
2021-06-23 20:52:24 -0300

    :sparkles: Created question component

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,3 +1,25 @@
-export function Question() {
-  return <h1>alou</h1>;
+import styles from "./styles.module.scss";
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+};
+
+export function Question({ content, author }: QuestionProps) {
+  return (
+    <div className={styles.question}>
+      <p>{content}</p>
+
+      <footer>
+        <div className={styles.userInfo}>
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div></div>
+      </footer>
+    </div>
+  );
 }
